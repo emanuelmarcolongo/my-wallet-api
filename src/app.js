6afe29c3a28fa718c55f3f2281fcf6ace9d449e2
Emanuel Marcolongo <emanuelmarcolongo@pop-os.localdomain>
2022-11-16 16:02:44 -0300

    created: dependencies + base config for app.js

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from 'uuid';
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+try {
+await mongoClient.connect();
+} catch (err) {
+console.log("Erro no mongo.conect", err.message);
+}
+
+db = mongoClient.db("myWallet");
+
+
+// ROTAS:
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
