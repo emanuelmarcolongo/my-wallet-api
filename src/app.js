2a109cd2ba9fd6eaee4d8b942f742f5d36f21b5e
Emanuel Marcolongo <emanuelmarcolongo@pop-os.localdomain>
2022-11-17 14:06:13 -0300

    feat: sign-in with authentications / creating userSession with token

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuidV4 } from 'uuid';
 
-import { postSignUp } from "./controllers/usersController.js";
+import { postSignIn, postSignUp } from "./controllers/usersController.js";
 
 const app = express();
 dotenv.config();
@@ -18,6 +18,11 @@ export const signupSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required().min(3),
     confirmpassword: joi.string().required().min(3)
+})
+
+export const signinSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
 })
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -32,10 +37,13 @@ console.log("Erro no mongo.conect", err.message);
 db = mongoClient.db("myWallet");
 export const usersCollection = db.collection("users");
 export const transactionsCollection = db.collection("transactions");
+export const sessionCollection = db.collection("sessions");
 
 // ROTAS:
 
 app.post("/sign-up", postSignUp);
+
+app.post("/sign-in", postSignIn);
 
 const port = 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,9 @@
-import { signupSchema, usersCollection } from "../app.js";
+import { sessionCollection, signinSchema, signupSchema, usersCollection } from "../app.js";
 import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from 'uuid';
+
+
+
 
 export async function postSignUp (req, res) {
 
@@ -36,5 +40,44 @@ export async function postSignUp (req, res) {
 }
 
 export async function postSignIn (req, res) {
+    const user = req.body;
+
+    const validation = signinSchema.validate(user, {abortEarly: false});
+
+    if(validation.error) {
+        const errors = validation.error.details.map((i) => i.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const userExists = await usersCollection.findOne ({ email: user.email});
+
+        if (!userExists) {
+            return res.status(404).send("E-mail não encontrado, por favor faça seu cadastro")
+        }
+    
+        if (bcrypt.compareSync (user.password, userExists.password)) {
+            
+            const session = await sessionCollection.findOne({userId: userExists._id})
+    
+            if (session) {
+                return res.status(201).send(session.token);
+            }
+    
+            const token = uuidV4();
+    
+            await sessionCollection.insertOne({
+                userId: userExists._id,
+                token
+            })
+    
+            return res.status(201).send(token);
+        } else {
+            return res.status(401).send("Senha incorreta")
+        }
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+    
 
 }
